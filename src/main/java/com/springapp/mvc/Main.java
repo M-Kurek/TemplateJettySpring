1834a03165e41f2069d16d9f2c0af8216004cbfa
gekoreed <gekoreed@gmail.com>
2015-01-22 18:03:21 +0200

    Initial commit

---
+++ src/main/java/com/springapp/mvc/Main.java
@@ -0,0 +1,27 @@
+package com.springapp.mvc;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.webapp.WebAppContext;
+
+/**
+ * Created by eshevchenko on 22.01.15 at 13:06.
+ */
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+
+        Server server = new Server(8080);
+
+        WebAppContext context = new WebAppContext();
+        context.setDescriptor("webapp/WEB-INF/web.xml");
+        context.setContextPath("/");
+        context.setResourceBase("src/main/webapp");
+        context.setParentLoaderPriority(true);
+
+        server.setHandler(context);
+
+        server.start();
+        server.join();
+    }
+
+}
