c44207ea12e4f37fa3bbee309728abdc4b4b0788
cgj647 <maciej.kurek@motorolasolutions.com>
2018-04-11 15:40:04 +0200

    pipeline DSL with scriptPath

--- src/main/groovy/pipeline_dsl.groovy
+++ src/main/groovy/pipeline_dsl.groovy
@@ -4,7 +4,7 @@ factory.pipelineJob("4th DSL create pipeline") {
     definition {
         cpsScm {
             scm {
-                git('https://github.com/jenkinsci/job-dsl-plugin.git')
+                git('https://github.com/M-Kurek/TemplateJettySpring.git')
             }
             scriptPath("src/main/groovy/pipelines_defs/pipeline_def_1.txt")
         }
